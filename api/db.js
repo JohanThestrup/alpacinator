13ba37040559256cc3ed63c93a60fd12eaa756f6
Johan Thestrup <johan.thestrup@playngo.com>
2022-03-01 01:20:22 +0100

    added production connection string

--- api/db.js
+++ api/db.js
@@ -1,6 +1,9 @@
 const pgp = require("pg-promise")();
-const db = pgp(
-    `postgres://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DB}`
-);
+const connectionString =
+    process.env.NODE_ENV === "production"
+        ? process.env.DATABASE_URL
+        : `postgres://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DB}`;
+
+const db = pgp(connectionString);
 
 module.exports = { db, pgp };
